46091d452466ad2d25d8d3de1a4ad2617f838a58
Koki Sakurai <sakurai.koki@e-jan.co.jp>
2022-11-09 12:51:16 +0900

    Create Java socket

--- src/SocketServer.java
+++ src/SocketServer.java
@@ -1,52 +1,53 @@
 import java.io.*;
-import java.net.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class SocketServer {
     public static void main(String[] args) {
-        //HTTP通信ポート番号80、HTTP通信ポート番号443
-        try (ServerSocket server = new ServerSocket()) {
-            while (true) {
-                server.bind(new InetSocketAddress("localhost", 8080));
+        //HTTP通信ポート番号80、HTTP通信ポート番号443、ポート8080番は代替HTTPポート
+        try (ServerSocket server = new ServerSocket(8080)){
+
+            while(true) {
+                //接続待ち
+                Socket socket = server.accept();
+                System.out.println("クライアントからの接続がありました。");
+
                 try {
-                    // -----------------------------------------
-                    // 2.クライアントからの接続を待ち受け（accept）
-                    // -----------------------------------------
-                    Socket sc = server.accept();
-                    System.out.println("クライアントからの接続がありました。");
-                    BufferedReader reader = null;
-                    PrintWriter writer = null;
-                    // -----------------------------------------
-                    // 3.クライアントからの接続ごとにスレッドで通信処理を実行
-                    // -----------------------------------------
-                    try {
-                        // クライアントからの受取用
-                        reader = new BufferedReader(new InputStreamReader(sc.getInputStream()));
-                        // サーバーからクライアントへの送信用
-                        writer = new PrintWriter(sc.getOutputStream(), true);
-                        // クライアントから「exit」が入力されるまで無限ループ
-                        String line = null;
-                        while (true) {
-                            // クライアントから送信されたメッセージを取得
-                            line = reader.readLine();
-                            if (line.equals("exit")) {
-                                break;
-                            }
-                            System.out.println("クライアントからのメッセージ＝" + line);
-                            writer.println("Please input:");
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                    String line = reader.readLine();
+                    StringBuilder header = new StringBuilder(); //header
+                    String body = null; //body
+                    int contentLength = 0;
+
+                    while (line != null && !line.isEmpty()) {
+                        if (line.startsWith("Content-Length")) {
+                            contentLength = Integer.parseInt(line.split(":")[1].trim());
                         }
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    } finally {
-                        // リソースの解放
-                        if (reader != null)
-                            reader.close();
-                        if (writer != null)
-                            writer.close();
-                        if (sc != null)
-                            sc.close();
+                        header.append(line + "\n");
+                        line = reader.readLine();
                     }
-                } catch (Exception ex) {
-                    ex.printStackTrace();
-                    break;
+
+                    if (0 < contentLength) {
+                        char[] c = new char[contentLength];
+                        reader.read(c);
+                        body = new String(c);
+                    }
+
+                    System.out.println(header);
+                    System.out.println(body);
+
+                    BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+                    writer.write("HTTP/1.1 200 OK\r\n");
+                    writer.write("Content-Length: " + ("The username is "+body).length() + "\r\n");
+                    writer.write("Connection: close\r\n"); //コネクションを切るかどうか?
+                    writer.write("\r\n");
+
+                    writer.write("The username is " + body);
+                    writer.flush();
+                    reader.close();
+                    socket.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
                 }
             }
         } catch (Exception e) {
